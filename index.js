cd7d1c5bd6bcd27f9bf2234132e5a9389ec9db44
muriloignaccio <murilohsignacio@gmail.com>
2021-05-25 21:13:11 -0300

    chore: add the division function

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 console.log("ola");
 
 function soma(a, b) {
-    return a + b
+  return a + b;
 }
 
 function subtracao(a, b) {
-    return a - b;
+  return a - b;
 }
 
+function divisao() {
+  return a / b;
+}
